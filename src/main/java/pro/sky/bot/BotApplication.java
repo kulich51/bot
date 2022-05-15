e7e9d73cba16131e601c032f10c9536e68a857c0
Samantha <sammy23051996@gmail.com>
2022-05-15 19:49:23 +0300

    demo new branch

--- src/main/java/pro/sky/bot/BotApplication.java
+++ src/main/java/pro/sky/bot/BotApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BotApplication {
-
+//samantha.danilkovich
 
     public static void main(String[] args) {
         SpringApplication.run(BotApplication.class, args);
