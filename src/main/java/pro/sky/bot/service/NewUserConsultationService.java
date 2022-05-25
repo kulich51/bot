3b396bb0a6e8ffb95c441d352f1f44225bf02fa0
kulich51 <59617932+kulich51@users.noreply.github.com>
2022-05-25 18:47:35 +0300

    Исправлены ошибки с Liquibase, по этапу 1 ТЗ добавлена клавиатура

--- src/main/java/pro/sky/bot/service/NewUserConsultationService.java
+++ src/main/java/pro/sky/bot/service/NewUserConsultationService.java
@@ -1,4 +1,8 @@
 package pro.sky.bot.service;
 
+import com.pengrad.telegrambot.request.BaseRequest;
+
 public interface NewUserConsultationService {
+
+    BaseRequest parse(Long chatId, String url);
 }
--- src/main/resources/liquibase/scripts/pet_shelter_db.sql
+++ src/main/resources/liquibase/scripts/pet_shelter_db.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset kulich51: tables contacts and user_appeal created
+--changeset kulich51: table contacts
 create table contacts(
     user_id bigint primary key,
     chat_id bigint,
@@ -7,9 +7,9 @@ create table contacts(
     first_name text
 );
 
-create table user_appeal(
-    id serial primary key,
-    user_id bigint,
-    question_solved bool,
-    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES contacts(user_id)
-);
+-- create table user_appeal(
+--     id serial primary key,
+--     user_id bigint,
+--     question_solved bool,
+--     CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES contacts(user_id)
+-- );
