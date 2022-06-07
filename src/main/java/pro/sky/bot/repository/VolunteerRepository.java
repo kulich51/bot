31899e26eca8f25763ac9b9e025411e5dec3bbec
Samantha <sammy23051996@gmail.com>
2022-06-07 17:14:50 +0300

    Добавлен метод добавления контакта и вызоваволонтера при попытке задать "Другой вопрос"

--- src/main/java/pro/sky/bot/repository/VolunteerRepository.java
+++ src/main/java/pro/sky/bot/repository/VolunteerRepository.java
@@ -1,12 +1,11 @@
 package pro.sky.bot.repository;
 
 import com.pengrad.telegrambot.model.Contact;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pro.sky.bot.model.Volunteer;
 
 import java.util.List;
 
 public interface VolunteerRepository extends JpaRepository<Volunteer, Long> {
-    List<Volunteer> findAll();
 }
